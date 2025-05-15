8eccd1413720a7925ae2c7e44ef97c9d669a2aed
André de Moraes Costa <an.moraescosta@gmail.com>
2025-05-15 15:57:56 -0300

    Fixed indentation

--- main.go
+++ main.go
@@ -17,48 +17,48 @@ type Page struct {
 }
 
 func (p *Page) save() error {
-	filename := p.Title + ".txt"
-	return os.WriteFile(filename, p.Body, 0600)
+    filename := p.Title + ".txt"
+    return os.WriteFile(filename, p.Body, 0600)
 }
 
 
 func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
-	body, err := os.ReadFile(filename)
+    filename := title + ".txt"
+    body, err := os.ReadFile(filename)
 
-	if err != nil {
-		return nil, err
-	}
+    if err != nil {
+        return nil, err
+    }
 
-	return &Page{Title: title, Body: body}, nil
+    return &Page{Title: title, Body: body}, nil
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	t, _ := template.ParseFiles("templates/" + tmpl + ".html")
-	t.Execute(w, p)
+    t, _ := template.ParseFiles("templates/" + tmpl + ".html")
+    t.Execute(w, p)
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/view/"):]
+    title := r.URL.Path[len("/view/"):]
 
-	p, err := loadPage(title)
+    p, err := loadPage(title)
     if err != nil {
         http.Redirect(w, r, "/edit/"+title, http.StatusFound)
         return
     }
 
-	renderTemplate(w, "view", p)
+    renderTemplate(w, "view", p)
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/edit/"):]
+    title := r.URL.Path[len("/edit/"):]
 
-	p, err := loadPage(title)
-	if err != nil {
-		p = &Page{Title: title}
-	}
+    p, err := loadPage(title)
+    if err != nil {
+        p = &Page{Title: title}
+    }
 
-	renderTemplate(w, "edit", p)
+    renderTemplate(w, "edit", p)
 }
 
 func saveHandler(w http.ResponseWriter, r *http.Request) {
